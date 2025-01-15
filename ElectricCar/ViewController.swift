2481076468247a1db501d5b577310d0710f34c30
Tardes <wellington.esteban.romero@gmail.com>
2025-01-15 17:48:24 +0100

    Initialized cocoaPods, implemented firebase

--- ElectricCar/ViewController.swift
+++ ElectricCar/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func createUser(_ sender: Any) {
+        Auth.auth().createUser(withEmail: usernameTextField.text!, password: passwordTextField.text!) { authResult, error in
+            if let error = error {
+                print(error)
+                
+                let alertController = UIAlertController(title: "Create user", message: error.localizedDescription, preferredStyle: .alert)
+                alertController.addAction(UIAlertAction(title: "OK", style: .default))
+                self.present(alertController, animated: true, completion: nil)
+                
+            } else {
+                print("user signs up succeddfuly")
+                
+                let alertController = UIAlertController(title: "Create user", message: "user signs up succeddfuly", preferredStyle: .alert)
+                alertController.addAction(UIAlertAction(title: "OK", style: .default))
+                self.present(alertController, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    @IBAction func SignIn(_ sender: Any) {
+        Auth.auth().signIn(withEmail: usernameTextField.text!, password: passwordTextField.text!) { [weak self] authResult, error in
+            if let error = error {
+                
+                let alertController = UIAlertController(title: "Sign in", message: error.localizedDescription, preferredStyle: .alert)
+                alertController.addAction(UIAlertAction(title: "OK", style: .default))
+                self!.present(alertController, animated: true, completion: nil)
+            } else {
+                let alertController = UIAlertController(title: "Sign in", message: "Sign In", preferredStyle: .alert)
+                alertController.addAction(UIAlertAction(title: "OK", style: .default))
+                self!.present(alertController, animated: true, completion: nil)
+                
+                self?.performSegue(withIdentifier: "goToHome", sender: nil)
+            }
+        }
+    }
 }
 
